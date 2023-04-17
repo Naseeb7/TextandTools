f54e2d7006896bef8e4e09a3527c6e000fb7c80d
Pradosh <chandpradosh7@gmail.com>
2023-04-17 18:14:16 +0530

    Fixed all errors for deploy

--- src/App.js
+++ src/App.js
@@ -4,42 +4,42 @@ import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 import Alert from './Components/Alert';
 import React, { useState } from 'react'
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-// } from "react-router-dom";
-// import About from './Components/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import About from './Components/About';
 
 
 function App() {
-  const green={
+  const green = {
     backgroundColor: "#A7BEAE",
     color: "black"
   }
-  const peach={
+  const peach = {
     backgroundColor: "#EEA47FFF",
     color: "#00539CFF"
   }
-  const red={
+  const red = {
     backgroundColor: "#990011FF",
     color: "#FCF6F5FF"
   }
-  const pink={
+  const pink = {
     backgroundColor: "#e52165 ",
     color: "#0d1137"
   }
-  const white={
+  const white = {
     backgroundColor: "white",
     color: "black"
   }
-  const dark={
+  const dark = {
     backgroundColor: "#323437",
     color: "#646669",
   }
-  const [mode,setmode]=useState(dark);
-  const [alert,setAlert]=useState(null);
-  const showAlert=(title,message)=>{
+  const [mode, setmode] = useState(dark);
+  const [alert, setAlert] = useState(null);
+  const showAlert = (title, message) => {
     setAlert({
       title: title,
       message: message
@@ -48,62 +48,62 @@ function App() {
       setAlert("")
     }, 2000);
   }
-  const handlethemes=()=>{
-    let greenbtn=document.getElementById("greenbtn")
-    let whitebtn=document.getElementById("whitebtn")
-    let peachbtn=document.getElementById("peachbtn")
-    let redbtn=document.getElementById("redbtn")
-    let pinkbtn=document.getElementById("pinkbtn")
-    let darkbtn=document.getElementById("darkbtn")
-    if(greenbtn.checked===true){
+  const handlethemes = () => {
+    let greenbtn = document.getElementById("greenbtn")
+    let whitebtn = document.getElementById("whitebtn")
+    let peachbtn = document.getElementById("peachbtn")
+    let redbtn = document.getElementById("redbtn")
+    let pinkbtn = document.getElementById("pinkbtn")
+    let darkbtn = document.getElementById("darkbtn")
+    if (greenbtn.checked === true) {
       setmode(green)
-      document.body.style.background="#A7BEAE"
-      showAlert("Applied!","MintGreen Theme")
+      document.body.style.background = "#A7BEAE"
+      showAlert("Applied!", "MintGreen Theme")
     }
-    else if(darkbtn.checked===true){
+    else if (darkbtn.checked === true) {
       setmode(dark)
-      document.body.style.background="#323437"
-      showAlert("Applied!","Dark Theme")
+      document.body.style.background = "#323437"
+      showAlert("Applied!", "Dark Theme")
     }
-    else if(peachbtn.checked===true){
+    else if (peachbtn.checked === true) {
       setmode(peach)
-      document.body.style.background="#EEA47FFF"
-      showAlert("Applied!","Peach Theme")
+      document.body.style.background = "#EEA47FFF"
+      showAlert("Applied!", "Peach Theme")
     }
-    else if(redbtn.checked===true){
+    else if (redbtn.checked === true) {
       setmode(red)
-      document.body.style.background="#990011FF"
-      showAlert("Applied!","CherryRed Theme")
+      document.body.style.background = "#990011FF"
+      showAlert("Applied!", "CherryRed Theme")
     }
-    else if(pinkbtn.checked===true){
+    else if (pinkbtn.checked === true) {
       setmode(pink)
-      document.body.style.background="#e52165"
-      showAlert("Applied!","Pink Theme")
+      document.body.style.background = "#e52165"
+      showAlert("Applied!", "Pink Theme")
     }
-    else if(whitebtn.checked===true){
+    else if (whitebtn.checked === true) {
       setmode(white)
-      document.body.style.background="white"
-      showAlert("Applied!","White Theme")
+      document.body.style.background = "white"
+      showAlert("Applied!", "White Theme")
     }
   }
   return (
     <>
-      {/* <Router> */}
-    <div style={mode}>
-    <Navbar title="TextandTools" aboutText="About" mode={mode} handlethemes={handlethemes}/>
-    <Alert alert={alert}/>
-    <div>
-    {/* <Switch>
-          <Route exact path="/"> */}
-            <TextForm showAlert={showAlert} heading="Enter your text to analyze" mode={mode}/>
-          {/* </Route>
-          <Route exact path="/about">
-            <About mode={mode}/>
-          </Route>
-        </Switch>*/}
-      </div> 
-      </div>
-    {/* </Router> */}
+      <Router>
+        <div style={mode}>
+          <Navbar title="TextandTools" aboutText="About" mode={mode} handlethemes={handlethemes} />
+          <Alert alert={alert} />
+          <div>
+            <Switch>
+              <Route exact path="/">
+                <TextForm showAlert={showAlert} heading="Enter your text to analyze" mode={mode} />
+              </Route>
+              <Route exact path="/about">
+                <About mode={mode} />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </Router>
     </>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,69 +2,68 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import "./Navbar.css"
 import Sliderbutton from "./Sliderbutton.js"
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 export default function Navbar(props) {
-  const handleSearch=(e)=>{
-    let preview=document.getElementById("preview").innerHTML
-    let searchvalue=document.getElementById("searchbar").value.trim()
-    if(searchvalue!==""){
-      let newword=new RegExp(searchvalue,"g"); // search for all instances
-		let newText = preview.replace(newword, `<mark>${searchvalue}</mark>`);
-    document.getElementById("preview").innerHTML=newText
-    const findAll = (arr, query) => {
-      let count = 0;
-      count = arr.filter(el => {
-         return el.indexOf(query) !== -1;
-      }).length;
-      return count;
-   };
-   if(preview!==""){
-     if(findAll(preview.split(" "),searchvalue)===0){
-       document.getElementById("occurances").innerHTML="Not in the text!"
+  const handleSearch = (e) => {
+    let preview = document.getElementById("preview").innerHTML
+    let searchvalue = document.getElementById("searchbar").value.trim()
+    if (searchvalue !== "") {
+      let newword = new RegExp(searchvalue, "g"); // search for all instances
+      let newText = preview.replace(newword, `<mark>${searchvalue}</mark>`);
+      document.getElementById("preview").innerHTML = newText
+      const findAll = (arr, query) => {
+        let count = 0;
+        count = arr.filter(el => {
+          return el.indexOf(query) !== -1;
+        }).length;
+        return count;
+      };
+      if (preview !== "") {
+        if (findAll(preview.split(" "), searchvalue) === 0) {
+          document.getElementById("occurances").innerHTML = "Not in the text!"
+        }
+        else {
+          document.getElementById("occurances").innerHTML = `Number of occurances : ${findAll(props.text.split(" "), searchvalue)}`
+          props.showAlert("See preview for results")
+        }
       }
-      else{
-        document.getElementById("occurances").innerHTML=`Number of occurances : ${findAll(props.text.split(" "),searchvalue)}`
-        props.showAlert("See preview for results")
-      }  
-    }
-    else{
-      props.showAlert("Enter text first!")
+      else {
+        props.showAlert("Enter text first!")
+      }
     }
   }
-}
   return (
     <>
-    <div className="navbarContainer">
-      <div className="links">
-      <label className='title' style={props.mode}>{props.title}</label>
-      <a href="#" className="links" id="homelink">Home</a>
-      {/* <Link to="/" className="links" id="homelink">Home</Link> */}
-      {/* <Link to="/about" className="links" id="aboutlink">{props.aboutText}</Link> */}
-      </div>
-      <div className="searchContainer" id='searchContainer'>
-        <input type="text" className="searchbar border" style={props.mode} name='search' id='searchbar'/>
-        <button className="btn border" id="searchbtn" style={props.mode} name='search' onClick={handleSearch}>Search</button>
-      </div>
+      <div className="navbarContainer">
+        <label className='title' style={props.mode}>{props.title}</label>
+        <div className="alllinks">
+          <Link to="/" className="links" id="homelink" style={props.mode}>Home</Link>
+          <Link to="/about" className="links" id="aboutlink" style={props.mode}>{props.aboutText}</Link>
+        </div>
+        <div className="searchContainer" id='searchContainer'>
+          <input type="text" className="searchbar border" style={props.mode} name='search' id='searchbar' />
+          <button className="btn border" id="searchbtn" style={props.mode} name='search' onClick={handleSearch}>Search</button>
+        </div>
       </div>
       <div className="themebtns">
         <div className="themetext heading">Make it fun :</div>
-      <Sliderbutton type={"radio"} heading={"MintGreen"} style={props.mode} name={"themebtn"} id={"greenbtn"} handlethemes={props.handlethemes}/>
-      <Sliderbutton type={"radio"} heading={"Peach"} style={props.mode} name={"themebtn"} id={"peachbtn"} handlethemes={props.handlethemes}/>
-      <Sliderbutton type={"radio"} heading={"CherryRed"} style={props.mode} name={"themebtn"} id={"redbtn"} handlethemes={props.handlethemes}/>
-      <Sliderbutton type={"radio"} heading={"Pink"} style={props.mode} name={"themebtn"} id={"pinkbtn"} handlethemes={props.handlethemes}/>
-      <Sliderbutton type={"radio"} heading={"White"} style={props.mode} name={"themebtn"} id={"whitebtn"} handlethemes={props.handlethemes}/>
-      <Sliderbutton type={"radio"} heading={"Dark"} style={props.mode} name={"themebtn"} id={"darkbtn"} handlethemes={props.handlethemes}/>
+        <Sliderbutton type={"radio"} heading={"MintGreen"} style={props.mode} name={"themebtn"} id={"greenbtn"} handlethemes={props.handlethemes} />
+        <Sliderbutton type={"radio"} heading={"Peach"} style={props.mode} name={"themebtn"} id={"peachbtn"} handlethemes={props.handlethemes} />
+        <Sliderbutton type={"radio"} heading={"CherryRed"} style={props.mode} name={"themebtn"} id={"redbtn"} handlethemes={props.handlethemes} />
+        <Sliderbutton type={"radio"} heading={"Pink"} style={props.mode} name={"themebtn"} id={"pinkbtn"} handlethemes={props.handlethemes} />
+        <Sliderbutton type={"radio"} heading={"White"} style={props.mode} name={"themebtn"} id={"whitebtn"} handlethemes={props.handlethemes} />
+        <Sliderbutton type={"radio"} heading={"Dark"} style={props.mode} name={"themebtn"} id={"darkbtn"} handlethemes={props.handlethemes} />
       </div>
     </>
   )
 }
-Navbar.propTypes={
-  title:PropTypes.string,
-  aboutText:PropTypes.string
+Navbar.propTypes = {
+  title: PropTypes.string,
+  aboutText: PropTypes.string
 }
-Navbar.defaultProps={
-  title:"Enter title",
-  aboutText:"About here"
+Navbar.defaultProps = {
+  title: "Enter title",
+  aboutText: "About here"
 };
